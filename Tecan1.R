5f2f807d429e92f7d8c3c39c445fef5c5a52da81
Sasha Levy <sashaflevy@gmail.com>
2018-06-01 13:58:06 -0700

    code3

--- Tecan1.R
+++ Tecan1.R
@@ -3,7 +3,24 @@
 setwd("~/OneDrive - Leland Stanford Junior University/Github/Tecan-Analysis/")
 
 #Import well IDs
-id = 
+well.id.file = "SAP30_ananalysis_Plate1_Well_IDs.txt"
+x = read.csv(well.id.file, header = F, row.names = NULL, col.names = NULL)
 
-#Set Import File
+#Import Tecan Data
+tecan.file = "2018-05-21_18-46_180521_Adam_Mia_4_96_T7.txt"
+x = read.csv(tecan.file, skip = 19)
+x = x[1:(nrow(x) - 2), ]
 
+#Matix of ODs
+od = as.matrix(x[,3:ncol(x)])
+
+#Vector of temperatures
+temp = x[,2]
+
+#Vector of Times (s)
+y = as.vector(x[,1])
+z = strsplit(z, "\t")[[1]][2]
+time = 1:length(z)
+for(i in 1:length(time)){
+  time[i] = as.numeric(z[[i]][2])
+}
